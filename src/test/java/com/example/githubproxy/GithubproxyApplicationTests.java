495649dcb3341700da3ba9e53f28a1c0b10e83ce
Kopcin <kopcin99@gmail.com>
2026-06-03 18:46:19 +0200

    Initial commit

---
+++ src/test/java/com/example/githubproxy/GithubproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
