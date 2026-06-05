25a41b87b7ed23df23af4deed6a6f6123746edff
Kopcin <kopcin99@gmail.com>
2026-06-05 22:01:28 +0200

    ...

--- src/main/java/com/example/githubproxy/GithubClient.java
+++ src/main/java/com/example/githubproxy/GithubClient.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
-@HttpExchange
+@HttpExchange("${github.api.base-url}")
 public interface GithubClient {
 
     @GetExchange("/users/{username}/repos")
